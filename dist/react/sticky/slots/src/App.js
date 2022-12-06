f94de823344e5de7e993429baa273ab593553458
Masood Abdolian <m.abdolian@gmail.com>
2022-12-06 21:46:52 +0330

    updated

--- dist/react/sticky/slots/src/App.js
+++ dist/react/sticky/slots/src/App.js
@@ -2,19 +2,7 @@ import { Sticky } from '@htmlplus/react';
 const StickySlots = () => {
   return (
     <div className="container">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nibh
-        eros, luctus in lacus eu, eleifend ultricies ipsum. Morbi sit amet diam
-        et erat pulvinar ultricies in vel erat. Vestibulum sit amet posuere
-        lacus, gravida semper libero. Praesent sed nisi sed lorem posuere
-        consequat. Nunc vehicula fermentum hendrerit. Mauris aliquam ornare
-        laoreet. Maecenas cursus nec ipsum et tempus. Sed pretium odio bibendum,
-        pharetra nisl sed, scelerisque ipsum. Nam egestas interdum risus et
-        gravida. Suspendisse aliquam leo ac leo fermentum, ac accumsan purus
-        vestibulum. Integer facilisis tincidunt urna vel accumsan. Orci varius
-        natoque penatibus et magnis dis parturient montes, nascetur ridiculus
-        mus. Maecenas et elit imperdiet, gravida urna tempus, fringilla nisl.
-      </p>
+      <br />
       <Sticky watcher>
         <header slot="normal">Normal Header</header>
         <header slot="stick">Stick Header</header>
