6c410c683a9ad2f13efbe8ff29ee61288bc2157d
Masood Abdolian <m.abdolian@gmail.com>
2024-03-17 00:03:19 +0330

    chore: Regenerate the examples

--- dist/javascript/tabs/reverse/main.js
+++ dist/javascript/tabs/reverse/main.js
@@ -1,3 +1,6 @@
+import '@htmlplus/core/faker.js';
 import '@htmlplus/core/tabs-bar.js';
+import '@htmlplus/core/tabs-panel.js';
+import '@htmlplus/core/tabs-panels.js';
 import '@htmlplus/core/tabs-tab.js';
 import '@htmlplus/core/tabs.js';
--- dist/svelte/tabs/reverse/src/App.svelte
+++ dist/svelte/tabs/reverse/src/App.svelte
@@ -1,13 +1,33 @@
 <script>
+  import '@htmlplus/core/faker.js';
   import '@htmlplus/core/tabs-bar.js';
+  import '@htmlplus/core/tabs-panel.js';
+  import '@htmlplus/core/tabs-panels.js';
   import '@htmlplus/core/tabs-tab.js';
   import '@htmlplus/core/tabs.js';
 </script>
 
-<plus-tabs>
+<plus-tabs value="tab-1">
   <plus-tabs-bar reverse>
-    <plus-tabs-tab> Tab 1 </plus-tabs-tab>
-    <plus-tabs-tab> Tab 2 </plus-tabs-tab>
-    <plus-tabs-tab> Tab 3 </plus-tabs-tab>
+    <plus-tabs-tab value="tab-1"> Tab 1 </plus-tabs-tab>
+    <plus-tabs-tab value="tab-2"> Tab 2 </plus-tabs-tab>
+    <plus-tabs-tab value="tab-3"> Tab 3 </plus-tabs-tab>
   </plus-tabs-bar>
+  <plus-tabs-panels>
+    <plus-tabs-panel value="tab-1">
+      <b>Tab One</b>
+      <br />
+      <plus-faker></plus-faker>
+    </plus-tabs-panel>
+    <plus-tabs-panel value="tab-2">
+      <b>Tab Two</b>
+      <br />
+      <plus-faker></plus-faker>
+    </plus-tabs-panel>
+    <plus-tabs-panel value="tab-3">
+      <b>Tab Three</b>
+      <br />
+      <plus-faker></plus-faker>
+    </plus-tabs-panel>
+  </plus-tabs-panels>
 </plus-tabs>
